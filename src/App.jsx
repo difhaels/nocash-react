4ad2ade4b6c76c4f44749f7f9f759bc509198648
Agung Saputra <agungsaputradifh@gmail.com>
2025-04-25 16:39:51 +0700

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Start from "./pages/Start";
+import Home from "./pages/Home";
+import FileEditor from "./pages/FileEditor";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
+    <Router>
+      <div className="min-h-screen bg-gray-100 text-gray-900">
+        <Routes>
+          <Route path="/" element={<Start />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/editor" element={<FileEditor />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card bg-red-200">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/FileEditor.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FileEditor() {
+  return (
+    <div>FileEditor</div>
+  )
+}
