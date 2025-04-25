0a35d125e0067d1c4d3a87c0bfb7f0c240f30376
Agung Saputra <agungsaputradifh@gmail.com>
2025-04-25 18:15:56 +0700

    start v1

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import { FolderDown, FolderUp } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
-export default function Start() {
+function Start() {
+  const navigate = useNavigate();
+
+  const handleCreate = () => {
+    // Logic buat create file baru (sementara arahkan ke Home)
+    navigate("/home");
+  };
+
+  const handleLoad = () => {
+    // Logic buat load file (sementara arahkan ke Home)
+    navigate("/home");
+  };
+
   return (
-    <div>Start</div>
-  )
+    <div className="flex flex-col items-center justify-center min-h-screen bg-yellow-400">
+      <h1 className="text-3xl font-bold mb-10">NOCASH</h1>
+
+      <div className="flex flex-col gap-4 w-40">
+        <div
+          onClick={handleCreate}
+          className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-sm cursor-pointer transition duration-300 flex justify-center-safe"
+        >
+          <FolderDown className="pr-2 w-8"/>
+          Create
+        </div>
+        <div
+          onClick={handleLoad}
+          className="bg-[#336AE9] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-sm cursor-pointer transition duration-300 flex justify-center-safe"
+        >
+          <FolderUp className="pr-2 w-8"/>
+          Load
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Start;
