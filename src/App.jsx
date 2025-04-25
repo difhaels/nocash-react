2004b2c7a0cd3ae2b2eb768e001c60ea96e30116
Agung Saputra <agungsaputradifh@gmail.com>
2025-04-25 16:29:16 +0700

    nocash

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
