105dce0ec4603e00ca92ae0f84f3d3b33eda94ca
Agung Saputra <agungsaputradifh@gmail.com>
2025-04-25 16:36:26 +0700

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <div>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="card bg-red-200">
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
